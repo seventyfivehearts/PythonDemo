34018718cfd9bea7dc751790d15be4332c2b5217
ASKTIME <pyecourse@gmail>
2021-02-10 18:15:36 +0800

    Dom操作

---
+++ 17 前端基础05-JS/01 JS变量和常量.js
@@ -0,0 +1,4 @@
+
+// 声明变量时使用 var ES6中使用let
+var name = 'tony'
+let name = 'tony'
---
+++ 17 前端基础05-JS/02 序列化.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/6 21:03
+# @Author  : sunzhen
+# @File    : 02 序列化.py
+# @Software: PyCharm
+import json
+json.JSONDecoder
